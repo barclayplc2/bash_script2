5edda67e95ed0b98d0dfd42ec7d35af48a360405
Cloud User <ec2-user@ip-172-31-19-147.ec2.internal>
2026-03-12 13:23:24 +0000

    my bash script

---
+++ my_commands.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+my_name="Josephine"
+num_of_script_yesterday=20
+num_of_script_today=10
+echo "Hey, i am actually creating folders and making magic happen ${my_name}.......hahaha1"
+echo "Yesterday i run ${num_of_script_yesterday}"
+echo "Today i am running ${num_of_script_today}"
+echo "In total i between yday and today i have run $((num_of_script_yesterday + num_of_script_today))"
+echo "for records sake, i am marking the date for today. And today's date is $(date) "
+cd
+mkdir shell_csripting
+touch i_am_learning_shell_script
+ls
+mv i_am_learning_shell_script shell_scripting/
+ls
+echo "still on the move engineer ${my_name}"
+mkdir action-runner
+tree
+sudo yum install tree
+sudo yum install tree -y
+sudo yum install git
+sudo yum install git -y
+history
+
+echo "this is yours truely Engineer ${my_name}"
+	
